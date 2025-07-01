c60ad42aaed9632b5493d3ebdd30832036c900e7
seguna15 <seguna15@gmail.com>
2025-07-01 12:29:48 +0100

    Configuration: Better Auth and Drizzle Config
    Install better-auth: npm install better-auth
    
    Install drizzle:  npm i drizzle-orm pg
    npm i -D drizzle-kit @types/pg
    
    Install better-auth cli: npm i @better-auth/cli
    
    Generate better-auth secret: npx @better-auth/cli@latest secret
    
    Drizzle migration: npx drizzle-kit migrate
    
    Generate with drizzle before migration: npx drizzle-kit generate

---
+++ src/lib/auth.ts
@@ -0,0 +1,44 @@
+import {betterAuth} from "better-auth";
+import {drizzleAdapter} from "better-auth/adapters/drizzle"
+import {db} from "@/lib/db";
+import * as schema from "@/lib/db/schema"
+
+
+export const auth = betterAuth({
+  appName: "Next Blog",
+  secret: process.env.BETTER_AUTH_SECRET || "BETTER_AUTH_SECRET",
+  baseURL: process.env.BASE_URL || "http://localhost:3000",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...schema,
+      user: schema.users,
+      session: schema.sessions,
+      account: schema.accounts,
+      post: schema.posts,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+    minPasswordLength: 6,
+    maxPasswordLength: 128,
+    autoSignIn: false,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5,
+    },
+    disableSessionRefresh: true,
+  },
+  advanced: {
+    useSecureCookies: process.env.NODE_ENV === "production",
+    defaultCookieAttributes: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
